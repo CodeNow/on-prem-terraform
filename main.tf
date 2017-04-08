e7343bfdf2faeb18a1cf4832501b93de5c8387eb
thejsj <jorge.silva@thejsj.com>
2017-04-07 18:06:40 -0700

    Change spacing. VIM is the best

--- main.tf
+++ main.tf
@@ -3,27 +3,27 @@ provider "aws" {
 }
 
 module "s3" {
-  source = "./s3"
-  domain = "${var.domain}"
+  source      = "./s3"
+  domain      = "${var.domain}"
   environment = "${var.environment}"
 }
 
 module "security-groups" {
-  source = "./security-groups"
+  source      = "./security-groups"
   environment = "${var.environment}"
 }
 
 module "database" {
-  source = "./database"
-  environment = "${var.environment}"
-  username = "${var.db_username}"
-  password = "${var.db_password}"
-  port = "${var.db_port}"
+  source            = "./database"
+  environment       = "${var.environment}"
+  username          = "${var.db_username}"
+  password          = "${var.db_password}"
+  port              = "${var.db_port}"
   subnet_group_name = "${var.db_subnet_group_name}"
 }
 
 module "instances" {
-  source = "./instances"
+  source      = "./instances"
   environment = "${var.environment}"
-  subnet_id = "${var.main_host_subnet_id}"
+  subnet_id   = "${var.main_host_subnet_id}"
 }
