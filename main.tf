c7e70f278a60105480f26f18d0584fea52ff1fb8
thejsj <jorge.silva@thejsj.com>
2017-04-07 17:13:26 -0700

    Add RDS, variables, and secrets

--- main.tf
+++ main.tf
@@ -12,3 +12,12 @@ module "security-groups" {
   source = "./security-groups"
   environment = "${var.environment}"
 }
+
+module "database" {
+  source = "./database"
+  environment = "${var.environment}"
+  db_username = "${var.db_username}"
+  db_password = "${var.db_password}"
+  db_port = "${var.db_port}"
+  db_subnet_group_name = "${var.db_subnet_group_name}"
+}
