c7546818d75e24e20e3984682446b3793e6360c3
Jorge Silva <jorge.silva@thejsj.com>
2017-04-10 12:24:13 -0700

    Add support for S3 buckets in terraform (#1)
    ### Commits
    
    * Add support for S3 buckets in terraform
    * Add public hosting

---
+++ s3/main.tf
@@ -0,0 +1,70 @@
+variable "domain" {}
+variable "environment" {}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "app.${var.domain}"
+  acl    = "public-read"
+
+  tags {
+    Description = "Bucket to host Angular application"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "${var.domain}"
+  acl    = "public-read"
+
+  tags {
+    Description = "Bucket to host FE site for logging in"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket" "runnable_vault" {
+  bucket = "runnable.vault.${var.environment}"
+  acl    = "private"
+
+  tags {
+    Description = "Bucket to server as a backend for vault"
+  }
+}
+
+resource "aws_s3_bucket" "context_resources" {
+  bucket = "runnable.context.resources.${var.environment}"
+  acl    = "private"
+
+  tags {
+    Description = "Bucket to save resources for contexts"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket" "deploy_keys" {
+  bucket = "runnable.deploykeys.${var.environment}"
+  acl    = "private"
+
+  tags {
+    Description = "Bucket to Github deploy keys"
+  }
+}
+
+resource "aws_s3_bucket" "container_logs" {
+  bucket = "${var.environment}.container-logs"
+  acl    = "private"
+
+  tags {
+    Description = "Bucket to save container logs"
+  }
+}
