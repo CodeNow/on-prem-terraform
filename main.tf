ff580b13551c660592fe4ecd0f1c6dbf29709af4
Jorge Silva <jorge.silva@thejsj.com>
2017-04-10 12:28:12 -0700

    Add ec2 instances (#4)
    ## Commits
    
    * Initial addition. Security groups
    * Add security groups
    * Add RDS, variables, and secrets
    * Add EC2 instances. Change variables
    * Change spacing. VIM is the best
    * Add VPC to security groups
    * Join Instances and SG. Needed reference from SG for instance
    * Add private IP to single-host instance

--- main.tf
+++ main.tf
@@ -3,9 +3,18 @@ provider "aws" {
 }
 
 module "s3" {
-  source = "./s3"
-  domain = "${var.domain}"
+  source      = "./s3"
+  domain      = "${var.domain}"
   environment = "${var.environment}"
+}
+
+module "database" {
+  source            = "./database"
+  environment       = "${var.environment}"
+  username          = "${var.db_username}"
+  password          = "${var.db_password}"
+  port              = "${var.db_port}"
+  subnet_group_name = "${var.db_subnet_group_name}"
 }
   
 module "security-groups" {
@@ -13,11 +22,10 @@ module "security-groups" {
   environment = "${var.environment}"
 }
 
-module "database" {
-  source = "./database"
+module "instances-and-security-groups" {
+  source      = "./instances-and-security-groups"
   environment = "${var.environment}"
-  db_username = "${var.db_username}"
-  db_password = "${var.db_password}"
-  db_port = "${var.db_port}"
-  db_subnet_group_name = "${var.db_subnet_group_name}"
+  vpc_id      = "${var.main_host_vpc_id}"
+  subnet_id   = "${var.main_host_subnet_id}"
+  private_ip  = "${var.main_host_private_ip}"
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+# General
+
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-west-2"
@@ -11,6 +13,8 @@ variable "environment" {
 variable "domain" {
   description = "Main domain for your Runnable installation. You should already own this domain and have certs for it."
 }
+
+# Databases
 
 variable "db_username" {
   description = "Username for RDS Postgres instance"
@@ -27,4 +31,18 @@ variable "db_port" {
 
 variable "db_subnet_group_name" {
   description = "Subnet in which database will be created"
+}
+
+# EC2 Instances
+
+variable "main_host_vpc_id" {
+  description = "VPC in which security groups and instance for main host will be created."
+}
+
+variable "main_host_subnet_id" {
+  description = "Subnet in which main host EC2 instance will be created. Subnet must be part of VPC in `main_host_vpc_id`"
+}
+
+variable "main_host_private_ip" {
+  description = "Private IP address in VPC for main-host. This is important because ip address is encoded in launch configuration for docks."
 }
